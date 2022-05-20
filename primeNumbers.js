5c21b6aa5ae510bcd91af4362680ebb66e94f875
trickroll <mdong185@gmail.com>
2022-05-20 12:02:55 +0800

    wip prime

---
+++ primeNumbers.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/54d512e62a5e54c96200019e/solutions
+
+
+function primeFactors(n){
+  for (let i=2, res="", f; i <= n; i++) {
+    f=0;
+    while (n%i == 0) {
+        f++; n/=i 
+    }
+    res += f ? "(" + ( f>1 ? i+"**"+f  : i ) +")" : ""
+  }
+  return res || "("+n+")"
+}
+
+
+console.log(primeFactors(7775460))
