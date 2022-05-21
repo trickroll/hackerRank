c47540870326c9309a1229206fc1ab768742d5c7
trickroll <mdong185@gmail.com>
2022-05-22 03:11:21 +0800

    may 21

--- readBook.js
+++ readBook.js
@@ -1,21 +1,12 @@
 // https://www.codewars.com/kata/570c560c15944a98e9000fd2/train/javascript
 
-function sc(words){
-    let min = 0
-    const dict = {}
-    const arr = words.toLowerCase().replace(/[^a-z\s]+/gi, '').split(" ")
-    arr.forEach(e => {
-        if (!dict[e]) {
-            for (let _ = 0; _ < e.length; _++){
-                min++
-            }
-            dict[e] = true
-        }
-        else {
-            min += 1
-        }
-    });
-    return min
+function sc(w){
+    let m = 0, d = {}, a = w.toLowerCase().replace(/[^a-z\s]/gi, '').split(" ")
+    a.forEach(e => {
+        m += !d[e] ? e.length : 1
+        d[e] = 1
+    })
+    return m
   }
 
 console.log(sc("Related Articles: Ruby Environment, Coffee Script Environment, Java Script Environment, Python Environment, Haskell Environment, Java Environment, Clojure Environment, .NET Environment."))
