fd078cb7f3fe93356275d21852e3ee9ff887544c
trickroll <mdong185@gmail.com>
2022-07-04 10:23:21 +0800

    pusher

---
+++ onlyOne.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+
+function check(a, x) {
+    // your code here
+    return a.includes(x)
+  }
+
+console.log(check([66, 101], 66))
