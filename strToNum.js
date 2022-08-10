f96feb51679dc029d6446edbc4590d72a268eb25
trickroll <mdong185@gmail.com>
2022-08-10 08:09:13 +0800

    really back

---
+++ strToNum.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+const stringToNumber = function(str){
+    let ans = Number(str)
+    return ans;
+  }
+
+console.log(stringToNumber("1234"))
